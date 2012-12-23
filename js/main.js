bdc9957e7b4c19a5d7bfb5c864b7f30191a95231
Saleem Shafi <saleemshafi@gmail.com>
2012-12-23 16:32:38 -0600

    made it possible to plug in more card sets, and added one for the 80% quran vocab

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-(function($) {
+(function(window, $) {
 	
-	var numLetters = 1;
+	var cardsets = [];
+	
+	function registerCardSet(name, cards) {
+		cardsets[name] = cards;
+	}
+	
+	function selectCardSet(name) {
+		if (cardsets[name] != undefined) {
+			cardSet = cardsets[name];
+		} else cardSet = cardsets["sample"];
+	}
 
 	function randomOneFrom(list) {
 		return list[Math.floor((Math.random()*list.length))];
@@ -18,108 +28,30 @@
 		return newList;
 	}
 
-	function ArabicLetters() {
-		this.letters = [ this.ALIF, this.BAA, this.TAA, this.THAA, this.JEEM, this.HAA, this.KHAA, this.DAAL,this.ZHAAL,   
-				            this.RAA, this.ZAA, this.SEEN, this.SHEEN, this.SAAD, this.DHAAD, this.TOY, this.DHOY, 
-				            this.AYN, this.GHAYN, this.FAA, this.QAAF, this.KAAF, this.LAAM, this.MIM, this.NOON, this.HA, 
-				            this.WOW, this.TAA_MARBOOTHAH, this.ALIF_MAKSOOR, this.YA];
-		this.vowels = [this.FATHATAAN, this.KASRATAAN, this.DAMMATAAN, this.FATHA, this.KASRA, this.DAMMA, this.SHADDAH, this.SUKOON];
+	
+	function StaticCards(cardMap) {
+		this.cards = [];
+		for (var question in cardMap) {
+			this.cards.push({"question":question, "answer":cardMap[question]});
+		}
 	}
-
-	$.extend(ArabicLetters.prototype, {
-		ALIF : '\u0627',
-		BAA : '\u0628',
-		TAA_MARBOOTHAH : '\u0629',
-		TAA : '\u062A',
-		THAA : '\u062B',
-		JEEM : '\u062C',
-		HAA : '\u062D',
-		KHAA : '\u062E',
-		DAAL : '\u062F',
-		ZHAAL : '\u0630',
-		RAA : '\u0631',
-		ZAA : '\u0632',
-		SEEN : '\u0633',
-		SHEEN : '\u0634',
-		SAAD : '\u0635',
-		DHAAD : '\u0636',
-		TOY : '\u0637',
-		DHOY : '\u0638',
-		AYN : '\u0639',
-		GHAYN : '\u063A',
-		FAA : '\u0641',
-		QAAF : '\u0642',
-		KAAF : '\u0643',
-		LAAM : '\u0644',
-		MIM : '\u0645',
-		NOON : '\u0646',
-		HA : '\u0647',
-		WOW : '\u0648',
-		ALIF_MAKSOOR : '\u0649',
-		YA : '\u064A',
-
-		FATHATAAN : '\u064B',
-		KASRATAAN : '\u064C',
-		DAMMATAAN : '\u064D',
-		FATHA : '\u064E',
-		KASRA : '\u064F',
-		DAMMA : '\u0650',
-		SHADDAH : '\u0651',
-		SUKOON : '\u0652',
-
-		setOptions: function(options) {
-			
-		},
-		
-		getTranscription : function(arabic) {
-			return "[answer goes here]";
-		},
-
-		generateVowel : function(isFirst, isLast, letter) {
-			var approVowels;
-			if (isFirst) {
-				approVowels = [this.FATHA, this.KASRA, this.DAMMA];
-			} else if (isLast) {
-				approVowels = removeFrom(this.vowels, this.SUKOON);
-			} else {
-				approVowels = removeFrom(this.vowels, [this.FATHATAAN, this.KASRATAAN, this.DAMMATAAN]);
-			}
-			var vowel = randomOneFrom(approVowels);
-			if (vowel == this.SHADDAH) {
-				vowel += randomOneFrom(removeFrom(approVowels, [this.SHADDAH, this.SUKOON]));
-			}
-			return vowel;
-		},
-		
-		generateLetter : function(isFirst, isLast) {
-			var approLetters;
-			if (isLast) {
-				approLetters = this.letters;
-			} else {
-				approLetters = removeFrom(this.letters, [this.TAA_MARBOOTHAH, this.ALIF_MAKSOOR]);
-			}
-			return randomOneFrom(approLetters);
-		}, 
-		
-		getNewArabic : function() {
-			var text = "";
-			for (var i = 0; i < numLetters; i++) {
-				var isFirst = i == 0;
-				var isLast = i+1 == numLetters;
-				var letter = this.generateLetter(isFirst, isLast);
-				text += letter + this.generateVowel( isFirst, isLast, letter );
-			}
-			return text;
-		},
-
+	
+	$.extend(StaticCards.prototype, {
 		getQuestionAnswer : function() {
-			var arabic = this.getNewArabic();
-			return { "question":arabic, "answer":this.getTranscription(arabic) };
+			return randomOneFrom(this.cards);
 		}
 	});
 
+	var sampleCards = {
+		"wahid": "one",
+		"ithnaan": "two",
+		"thalaatha": "three",
+		"arba3a": "four"
+	};
 	
-	var cardSet = new ArabicLetters();
+	
+	registerCardSet("sample", new StaticCards(sampleCards));
+	selectCardSet("sample");
 	
 	$("#workarea .nextBtn").click( function() { 
 		$("#workarea").addClass("testing");
@@ -132,7 +64,7 @@
 		$("#workarea").removeClass("testing");
 	});
 	
-	$("#options #numLetters").change(function(e) {
-		numLetters = $("#options #numLetters option:selected").val();
-	});
-}(jQuery));
+	window.registerCardSet = registerCardSet;
+	window.selectCardSet = selectCardSet;
+	window.StaticCards = StaticCards;
+}(window, jQuery));
---
+++ js/tsv2js.py
@@ -0,0 +1,28 @@
+import sys
+import codecs
+
+def convertToJSON(line):
+	pieces = line.split("\t");
+	return '    "'+pieces[0].strip()+'" : "'+pieces[1].strip()+'",\n'
+
+setName = sys.argv[1]
+inputFilename = setName+".tsv"
+outputFilename = "cards."+setName+".js"
+
+input = codecs.open(inputFilename, 'r', encoding='utf-8')
+output = codecs.open(outputFilename, 'w', encoding='utf-8')
+
+output.write("(function(window, $) {\n");
+output.write("	window.registerCardSet('"+setName+"', new StaticCards({\n");
+
+for line in input:
+	output.write(convertToJSON(line))
+
+output.write("  }));\n");
+output.write("	window.selectCardSet('"+setName+"');\n");
+output.write("}(window, jQuery));\n");
+
+
+
+input.close();
+output.close();
